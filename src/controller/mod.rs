98900d5854444740dd4fcc84da8474956f7a193e
abhivadya1 <abhivadya1@gmail.com>
2026-03-31 20:44:18 +0700

    Implement the receive function in Notification controller.

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -5,7 +5,8 @@ pub fn route_stage() -> AdHoc {
     AdHoc::on_ignite("Initializing controller routes...", |rocket| async {
         rocket.mount("/", routes![
             notification::subscribe,
-            notification::unsubscribe
+            notification::unsubscribe,
+            notification::receive
         ])
     })
 }
